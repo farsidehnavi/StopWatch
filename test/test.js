2e2fa226b8ab9afcb87a436520a42538634681e1
farsidehnavi <154635498+farsidehnavi@users.noreply.github.com>
2024-05-07 23:16:39 +0330

    The desine of upper 360px is done, the bog of removing Mark button is fixed, 4.0.1

---
+++ test/test.js
@@ -0,0 +1,23 @@
+// function b(){
+//     window.open('test2.html')
+// }
+// function f(){
+// }
+
+// let myWindow;
+
+// function openWin() {
+//   myWindow = window.open("stopwatch.html", "", "width=200, height=100");
+// }
+// function resizeWin() {
+//   myWindow.resizeTo(375,600);
+// }
+// export function done(){console.log('Done !')}
+// export function next(){
+//   console.log('next ...')
+// }
+
+let a=10
+export function a20(){a=20}
+export function a30(){a+10}
+export function ac(){console.log(a)}
