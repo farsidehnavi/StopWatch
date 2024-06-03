3081d8ded2d150b6d4a7ab00ac2d28b3d9b49e85
farsidehnavi <154635498+farsidehnavi@users.noreply.github.com>
2024-06-03 18:17:22 +0330

    Mobile phone desine bogs fixed

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ document.querySelector('.open').addEventListener('click',()=>{OpenStopWatch()})
 document.querySelector('.resize').addEventListener('click',()=>{ResizeStopWatch()})
 let stopwatch;
 function OpenStopWatch(){
-    stopwatch=window.open('stopwatch.html','','width=377, height=450')
-    // window.open('index.html','','width=400, height=800')// 'width=1300, height=800'
+    // stopwatch=window.open('stopwatch.html','','width=377, height=450')
+    window.open('index.html','','width=400, height=800')// 'width=1300, height=800'
     // window.open('settings.html','','width=340, height=225')
     // window.open('about.html','','width=650, height=500')
 }
